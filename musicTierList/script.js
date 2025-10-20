e3d359590933ab93ca5d0d012fbb61b7425a15e5
Dechawn Baker <dechawnz@gmail.com>
2025-10-20 17:10:03 -0400

    Added tierlist thing

---
+++ musicTierList/script.js
@@ -0,0 +1,28 @@
+async function loadSongs(){
+    try{
+        const response = await fetch('songs.json');
+        const data = await response.json();
+        data.songs.forEach(song => {
+            const img = document.createElement('img');
+            img.src = song.imageLink;
+            img.alt = song.title;
+            img.title = song.title;
+            img.classList.add('song-image');
+
+            const tierDiv = document.querySelector(`#${song.tier} .tierSort`);
+
+            if(tierDiv){
+                tierDiv.appendChild(img);
+            } else{
+                console.warn(`Tier ${song.tier} not found for song: ${song.title}`);
+            }
+        });
+    } catch(err){
+        console.error(err);
+    }
+}
+
+loadSongs();
+
+
+
