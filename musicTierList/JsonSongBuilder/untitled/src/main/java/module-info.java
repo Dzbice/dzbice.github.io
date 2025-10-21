1c8550dde458f681e7767704d07108be10376e0c
Dechawn Baker <dechawnz@gmail.com>
2025-10-20 22:17:11 -0400

    added songs

--- musicTierList/JsonSongBuilder/untitled/src/main/java/module-info.java
+++ musicTierList/JsonSongBuilder/untitled/src/main/java/module-info.java
@@ -1,2 +1,11 @@
-module $MODULE_NAME$ {
+module com.Dzbice.untitled {
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires com.google.gson;
+
+    // Export the package containing your main JavaFX Application
+    exports app.Dz;
+
+    // Open the package to javafx.graphics for reflective access
+    opens app.Dz to javafx.graphics;
 }
